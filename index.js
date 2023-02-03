8a9459c663157c3bb3bdca4e2fdc8f6a2f576232
Beata Kosmider <kosmibe@gmail.com>
2023-02-03 13:05:12 +0100

    task is done

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const colorizeElement = (node) => {
+  node.classList.add("new-class");
+  node.setAttribute("data-tag", node.tagName);
+  node.style.fontSize = "12px";
+  node.style.color = "#f1f1f1";
+  return node;
   // Zaimplementuj zadanie zgodnie z wytycznymi
 };
 
